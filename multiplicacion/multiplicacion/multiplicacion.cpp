d9317069fec33cde0508b2a4dc7b5336b5211fae
AndyRobGue <andilove28@outlook.com>
2022-11-29 13:47:19 -0600

    Declaramos las variables numeros y multiplicacion

--- multiplicacion/multiplicacion/multiplicacion.cpp
+++ multiplicacion/multiplicacion/multiplicacion.cpp
@@ -1,8 +1,14 @@
 
 #include <stdio.h>
 
-int main()
+void main(void)
 {
-    
+    /* Escribe un programa que defina un vector de números y calcule la
+multiplicación acumulada de sus elementos*/
+
+    int numeros[5] = { 1,2,3,4,5 };
+    int multiplicación = 1;
+
+    // Declaramos las variables numeros y multiplicacion
 }
 
